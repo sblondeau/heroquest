1e935897c6768889e1ae7d459f5a27f8a7d45ed4
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-02-22 18:02:48 +0100

    create entity

---
+++ src/Entity/Hero.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HeroRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=HeroRepository::class)
+ */
+class Hero
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $x;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $y;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getX(): ?int
+    {
+        return $this->x;
+    }
+
+    public function setX(int $x): self
+    {
+        $this->x = $x;
+
+        return $this;
+    }
+
+    public function getY(): ?int
+    {
+        return $this->y;
+    }
+
+    public function setY(int $y): self
+    {
+        $this->y = $y;
+
+        return $this;
+    }
+}
---
+++ src/Repository/HeroRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Hero;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Hero|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Hero|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Hero[]    findAll()
+ * @method Hero[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class HeroRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Hero::class);
+    }
+
+    // /**
+    //  * @return Hero[] Returns an array of Hero objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('h')
+            ->andWhere('h.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('h.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Hero
+    {
+        return $this->createQueryBuilder('h')
+            ->andWhere('h.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
